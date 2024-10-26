045e29953d7863552669eae57f532ca64302ef47
Stephen Iles <stephenciles@gmail.com>
2024-10-26 07:31:46 +0100

    added shopping bag app, urls and templates

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'allauth.account', # allauth package
     'allauth.socialaccount', # allauth package
     'home',
-    'products'
+    'products',
+    'bag',
 ]
 
 MIDDLEWARE = [
